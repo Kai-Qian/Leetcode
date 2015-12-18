9b49758c6d347c3af9da87587a6875e8062d7b57
Kai-Qian <kaiq@andrew.cmu.edu>
2015-12-18 17:40:52 -0500

    Add some codes

---
+++ String and Array/Roman to Integer.java
@@ -0,0 +1,61 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+ */
+
+public class Solution {
+    public int romanToInt(String s) {
+        int amount = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == 'I') {
+                if ((i + 1) < s.length() && s.charAt(i + 1) == 'V') {
+                    amount += 4;
+                    i = i + 2;
+                } else if ((i + 1) < s.length() && s.charAt(i + 1) == 'X') {
+                    amount += 9;
+                    i = i + 2;
+                } else {
+                    amount += 1;
+                    i = i + 1;
+                }
+            } else if (s.charAt(i) == 'X') {
+                if ((i + 1) < s.length() && s.charAt(i + 1) == 'L') {
+                    amount += 40;
+                    i = i + 2;
+                } else if ((i + 1) < s.length() && s.charAt(i + 1) == 'C') {
+                    amount += 90;
+                    i = i + 2;
+                } else {
+                    amount += 10;
+                    i = i + 1;
+                }
+            } else if (s.charAt(i) == 'C') {
+                if ((i + 1) < s.length() && s.charAt(i + 1) == 'D') {
+                    amount += 400;
+                    i = i + 2;
+                } else if ((i + 1) < s.length() && s.charAt(i + 1) == 'M') {
+                    amount += 900;
+                    i = i + 2;
+                } else {
+                    amount += 100;
+                    i = i + 1;
+                }
+            } else if (s.charAt(i) == 'V') {
+                amount += 5;
+                i = i + 1;
+            } else if (s.charAt(i) == 'L') {
+                amount += 50;
+                i = i + 1;
+            } else if (s.charAt(i) == 'D') {
+                amount += 500;
+                i = i + 1;
+            } else if (s.charAt(i) == 'M') {
+                amount += 1000;
+                i = i + 1;
+            }
+        }
+        return amount;
+    }
+}
